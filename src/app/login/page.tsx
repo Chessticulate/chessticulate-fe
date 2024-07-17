d98c1ae127f440a64f2841b73b826cd52f722db2
Brian Glaws <glaws.brian@gmail.com>
2024-07-16 21:49:54 -0400

    seperated login/signup into seperate page and form components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,49 +1,11 @@
-"use client";
-
 import Image from "next/image";
 import Navbar from "../../components/navbar";
+import Login_Form from "./login_form";
 import Link from "next/link";
 import { useState, ChangeEvent, FormEvent } from "react";
 import config from "../../../config.js";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(`${config.chessticulate_api_url}/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: username,
-          password: password,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      console.log("Success:", data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
   return (
     <div className="min-h-screen flex flex-col">
       <header className="flex justify-between items-center px-4 py-2 bg-gray-800 text-white">
@@ -55,35 +17,7 @@ export default function Login() {
         </div>
       </header>
       <main className="flex justify-center pt-10">
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col space-y-4 items-center"
-        >
-          <div className="flex flex-col space-y-center">
-            <div className="flex flex-col items-center pb-5 text-black">
-              <input
-                type="text"
-                placeholder="username"
-                value={username}
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <div className="flex flex-col items-center text-black">
-              <input
-                type="text"
-                placeholder="password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <button
-              type="submit"
-              className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md"
-            >
-              Log in
-            </button>
-          </div>
-        </form>
+        <Login_Form />
       </main>
     </div>
   );
---
+++ src/app/signup/signup_form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Image from "next/image";
+import Navbar from "../../components/navbar";
+import Link from "next/link";
+import { useState, ChangeEvent, FormEvent } from "react";
+import config from "../../../config.js";
+
+export default function Signup_Form() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleTokenChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${config.chessticulate_api_url}/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: username,
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setToken(data);
+      console.log("Success:", data);
+      console.log("setToken", token);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <div className="pb-5 text-black">
+              <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="pb-5 text-black">
+              <input
+                type="text"
+                placeholder="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="text-black">
+              <input
+                type="text"
+                placeholder="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md"
+            >
+              Sign up
+            </button>
+            <div className="flex flex-col items-center pt-6">
+              Already have an account?
+              <Link href="/login" passHref className="text-blue-500">
+                Log in
+              </Link>
+            </div>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
